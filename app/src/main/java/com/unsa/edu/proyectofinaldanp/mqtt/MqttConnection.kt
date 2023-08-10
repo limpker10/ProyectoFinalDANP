e9091b62901fd70905ecf10f9f0bd1ecd1aa2511
Jose Cano <jcanov@unsa.edu.pe>
2023-08-10 07:00:13 -0500

    agregando certificados proporcionados por AWS Iot Core

--- app/src/main/java/com/unsa/edu/proyectofinaldanp/mqtt/MqttConnection.kt
+++ app/src/main/java/com/unsa/edu/proyectofinaldanp/mqtt/MqttConnection.kt
@@ -3,6 +3,7 @@ package com.example.dnap_finalproject.mqtt
 import android.content.Context
 import android.util.Log
 import com.unsa.edu.proyectofinaldanp.mqtt.SensorDataTemperature
+import org.json.JSONException
 import org.json.JSONObject
 import software.amazon.awssdk.crt.CRT
 import software.amazon.awssdk.crt.mqtt.MqttClientConnection
@@ -12,51 +13,52 @@ import software.amazon.awssdk.crt.mqtt.QualityOfService
 import software.amazon.awssdk.iot.AwsIotMqttConnectionBuilder
 import java.io.FileOutputStream
 import java.nio.charset.StandardCharsets
+import java.util.UUID
 
 class MqttConnection(private val context: Context) {
     private val endPoint = "a2axh58ph1yz86-ats.iot.us-east-1.amazonaws.com"
-    private val rootCa = "Amazon.pem"
-    private val cert = "certificate.pem.crt"
-    private val privateKey = "private.pem.key"
+    private val resourceNames = listOf("Amazon.pem", "certificate.pem.crt", "private.pem.key")
 
     private val resourceMap: HashMap<String, String> by lazy { createResourceMap() }
     private val mqttClientConnection: MqttClientConnection by lazy { createMqttClientConnection() }
 
+    private val mqttClientCallbacks = object : MqttClientConnectionEvents {
+        override fun onConnectionInterrupted(errorCode: Int) {
+            if (errorCode != 0) {
+                Log.i("AWS IOT", "Connection interrupted: $errorCode: " + CRT.awsErrorString(errorCode))
+            }
+        }
+
+        override fun onConnectionResumed(sessionPresent: Boolean) {
+            Log.i("AWS IOT", "Connection resumed: " + if (sessionPresent) "existing session" else "clean session")
+        }
+    }
+
+    private fun copyResourceToCache(resourceName: String): String {
+        context.resources.assets.open(resourceName).use { res ->
+            val cachedName = "${context.externalCacheDir}/$resourceName"
+            FileOutputStream(cachedName).use { cachedRes ->
+                res.copyTo(cachedRes)
+            }
+            return cachedName
+        }
+    }
+
     private fun createResourceMap(): HashMap<String, String> {
-        val resourceNames = listOf(rootCa, cert, privateKey)
         val resourceMap = HashMap<String, String>()
         for (resourceName in resourceNames) {
-            context.resources.assets.open(resourceName).use { res ->
-                val cachedName = "${context.externalCacheDir}/${resourceName}"
-                FileOutputStream(cachedName).use { cachedRes ->
-                    res.copyTo(cachedRes)
-                }
-                resourceMap[resourceName] = cachedName
-            }
+            resourceMap[resourceName] = copyResourceToCache(resourceName)
         }
         return resourceMap
     }
 
     private fun createMqttClientConnection(): MqttClientConnection {
         val builder = AwsIotMqttConnectionBuilder.newMtlsBuilderFromPath(
-            resourceMap[cert], resourceMap[privateKey]
+            resourceMap["certificate.pem.crt"], resourceMap["private.pem.key"]
         )
 
-        val callbacks = object : MqttClientConnectionEvents {
-            override fun onConnectionInterrupted(errorCode: Int) {
-                if (errorCode != 0) {
-                    Log.i("AWS IOT", "Connection interrupted: $errorCode: " + CRT.awsErrorString(errorCode))
-                }
-            }
-
-            override fun onConnectionResumed(sessionPresent: Boolean) {
-                Log.i("AWS IOT", "Connection resumed: " + if (sessionPresent) "existing session" else "clean session")
-            }
-        }
-
-        builder
-            .withConnectionEventCallbacks(callbacks)
-            .withClientId("iotconsole-eeea290f-5d8d-4c76-aed9-f39d38d78f83")
+        builder.withConnectionEventCallbacks(mqttClientCallbacks)
+            .withClientId("iotconsole-${UUID.randomUUID()}")
             .withEndpoint(endPoint)
             .withPort(8883)
             .withCleanSession(true)
@@ -75,7 +77,8 @@ class MqttConnection(private val context: Context) {
     }
 
     fun publishMessage(message: String, topic: String, qos: Int) {
-        val publish = mqttClientConnection.publish( MqttMessage(topic, message.toByteArray(), QualityOfService.AT_LEAST_ONCE, false))
+        val mqttMessage = MqttMessage(topic, message.toByteArray(), QualityOfService.AT_LEAST_ONCE, false)
+        val publish = mqttClientConnection.publish(mqttMessage)
         try {
             publish.get()
         } catch (ex: Exception) {
@@ -88,15 +91,17 @@ class MqttConnection(private val context: Context) {
         subscribeFuture.whenComplete { _, throwable ->
             if (throwable == null) {
                 Log.i("MqttConnection", "Subscribed to topic: $topic")
-                mqttClientConnection.subscribe("esp8266/sub", QualityOfService.AT_MOST_ONCE){ message ->
-                        val payload = String(message.payload, StandardCharsets.UTF_8)
-                        Log.i("MqttConnection", "Received message: $payload")
-                        // Parsear JSON y obtener los valores de humedad y temperatura
+                mqttClientConnection.subscribe("esp8266/sub", QualityOfService.AT_MOST_ONCE) { message ->
+                    val payload = String(message.payload, StandardCharsets.UTF_8)
+                    Log.i("MqttConnection", "Received message: $payload")
+                    try {
                         val json = JSONObject(payload)
                         val humidity = json.getDouble("humidity")
                         val temperature = json.getDouble("temperature")
-                        // Actualizar los valores de humedad y temperatura
                         sensorDataTemperature.updateTemperature(temperature)
+                    } catch (e: JSONException) {
+                        Log.e("MqttConnection", "Error parsing JSON: $e")
+                    }
                 }
             } else {
                 Log.e("MqttConnection", "Failed to subscribe to topic: $topic", throwable)
